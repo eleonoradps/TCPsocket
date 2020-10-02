0dd730266484caf8a6a8a27b415adad53316f122
L <eleonora.dups@gmail.com>
2020-10-02 19:26:20 +0200

    tried doing threading + changed socket to response

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 import socket
+import sys
+import threading
 
 # server listens to everything happening on the port, when client and server both connected
 # they'll be able to communicate and send data
 
+class Client:
+    def __init__(self, conn):
+        threading.Client.__init__(self)
+        self.conn = conn
+
+    def run(self):  # showing the info
+        data = self.conn.recv(1024)
+        data = data.decode("utf8")
+        print(data)
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host, port = ('', 5656)
@@ -17,9 +28,12 @@ while True:  # make the server listen to the port
 
     data = conn.recv(1024)  # data will be received by connection
     # then we decode the data
-    socket = data.decode("utf8")
+    response = data.decode("utf8")
     # then we show(print) what the server got from the client
     print(data)
+
+
+start_new_thread(threaded(conn,))
 
 # when you create a socket and open a connection, you need to close them both
 
